b70ad13aaf665d1129bec19e1376b4f23e698605
sofiamorseletto <sofia.morseletto@mercadolibre.com>
2018-10-21 22:00:52 -0300

    #55 Fixed background notifications and added topic subscription

--- app/src/main/java/com/matacos/mataco/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/matacos/mataco/MyFirebaseMessagingService.kt
@@ -3,11 +3,9 @@ package com.matacos.mataco
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.IBinder
 import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -18,13 +16,8 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private val TAG: String = MyFirebaseMessagingService::class.java.simpleName
-    private val CHANNEL_ID: String = "exam_notifications"
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.from!!)
 
         // Check if message contains a data payload.
@@ -39,26 +32,21 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 //                handleNow()
             }
 
+            showNotification(remoteMessage.data!!["title"]!!, remoteMessage.data!!["body"]!!, remoteMessage.data!!["click_action"]!!, remoteMessage.data!!["channel_id"]!!)
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
-            showNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
+    private fun createNotificationChannel(channelId: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.exams_channel)
+            val name = getString(R.string.exams_channel)//TODO: cambiar esto segun el channel id
             val descriptionText = getString(R.string.exams_channel_desc)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            val channel = NotificationChannel(channelId, name, importance).apply {
                 description = descriptionText
             }
             // Register the channel with the system
@@ -68,17 +56,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun showNotification(title: String, body: String){
-        createNotificationChannel()
+    private fun showNotification(title: String, body: String, clickAction: String, channelId: String){
+        Log.d(TAG, "Channel id: $channelId")
+        createNotificationChannel(channelId)
 
-        val intent = Intent(this, MyExamsActivity::class.java)
+        Log.d(TAG, "Click action: $clickAction")
+        val intent = Intent(clickAction)
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 //        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 
 
-        var mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_notification_exam_cancelled)
+        var mBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
